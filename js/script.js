c7124b4beeaafd59a9e30460678c9acdf6a48759
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-06-07 22:07:26 +0000

    u

--- js/script.js
+++ js/script.js
@@ -20,11 +20,10 @@ function numberAddition() {
   // initializes sum value
   let sum = 0;
 
-  // while counter is not greater than the userMax, update the sum to equal the user number plus the counter, the display message to equal the sum, and the counter to equal the counter plus one. repeats until the counter is greater than the userMax
-  while (counter <= userMax) {
-    sum =  userInt + counter;
-    displayMessage = displayMessage + counter + "+" + userInt + "=" + sum + "<br>";
-    counter++;
+  // Check if any input is empty
+  if (userMax == "" || userMin == "" || userInt == "") {
+    document.getElementById("user-info").innerHTML = "Please fill all boxes";
+    return;
   }
 
   // if the user minimum number for the addition table is greater than their maximum number, it dislays an error message because a minimum should not be greater than a maximum. 
@@ -32,8 +31,13 @@ function numberAddition() {
     // displays error message in the display div
     document.getElementById("user-info").innerHTML = "Your minimum is greater than your maximum. Please try again.";
   } else {
+    // while counter is not greater than the userMax, update the sum to equal the user number plus the counter, the display message to equal the sum, and the counter to equal the counter plus one. repeats until the counter is greater than the userMax
+    while (counter <= userMax) {
+      sum =  userInt + counter;
+      displayMessage = displayMessage + counter + "+" + userInt + "=" + sum + "<br>";
+      counter++;
+    }
     // display the display message
     document.getElementById("user-info").innerHTML = displayMessage;
   }
 }
-
